ed7fd79ae67b7ebc90dd45e92fd0aa8e96cc3e9e
perywrld <ydb.wears@icloud.com>
2026-04-28 08:26:35 -0700

    updated content

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ export default function App() {
   const products = [
     { id: 1, name: "YDB Hoodie", price: "$80", img: "https://via.placeholder.com/300" },
     { id: 2, name: "YDB Classic Tee", price: "$40", img: "https://tee.png.joeg" },
-    { id: 3, name: "YDB: You Dont Belong Tee", price: "$25", img: "https://teee.png.jpeg" },
+    { id: 3, name: "YDB: You ", price: "$25", img: "https://teee.png.jpeg" },
+    { id: 3, name: "YDB: You ", price: "$25", img: "https://teee.png.jpeg" },
+    { id: 3, name: "YDB: Misfit Beanie ", price: "$10", img: "https://bean.jpeg" },
+    { id: 3, name: "YDB: You're Unwanted Tee ", price: "$25", img: "https://unwantedtee.jpeg" },
   ];
 
   return (
@@ -54,26 +57,7 @@ export default function App() {
         <h2>Join the Movement</h2>
         <input type="email" placeholder="Enter your email" />
         <button>Sign Up</button>
-      </section><form 
-  action="https://e45f8d25.sibforms.com/serve/MUIFAAuwLS9c6uu-nfzPdeMKCojGZ3DeE2E8dUqx3HMhajZulVKbzWrkwRYWFkgzwI3ATOAX4F50iwCDQpC6IbQu7g1doqU0WqQ2upMiW1zjhNvUUwLc4XUKTcrDyDj0E-dneUmvAo71uOGKJ_AKIDe546Z1Sbr5oAIYj_VrfQfK4sHyZ4ox7r8UYocFcK2eh5pDEw6HlFziVJCoIA==" 
-  method="POST"
-  target="_blank"
-  className="signup"
->
-  <h2>Join YDB</h2>
-  <p>Be the first to access new drops</p>
-
-  <input 
-    type="email" 
-    name="EMAIL" 
-    placeholder="Enter your email" 
-    required 
-  />
-
-  <button type="submit">
-    Join the outsiders
-  </button>
-</form>
+      </section>
 
       {/* FOOTER */}
       <footer>
